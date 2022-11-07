3fa1a71f8459ef68d8e5f6bcb80f8aad5c5a50a2
jerome.hou <jerome.hou@jkos.com>
2022-11-07 17:01:58 +0800

    feat: Completed

---
+++ LeetCode-1323/LeetCode-1323.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int maximum69Number(int num) {
+    char numStr[10] = "";
+    sprintf_s(numStr, "%d", num);
+    
+    for (int i = 0; i < sizeof(numStr); ++i) {
+        if (numStr[i] == '6') {
+            numStr[i] = '9';
+            break;
+        }
+    }
+
+    return atoi(numStr);
+}
+
+int main()
+{
+    int num = 9669;
+
+    printf("%d", maximum69Number(num));
+}
+
